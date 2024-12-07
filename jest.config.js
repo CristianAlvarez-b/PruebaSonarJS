b0b2c03a1ff223d642ba3d11fcdbba30c2dbfb79
CristianAlvarez-b <cristian.alvarez-b@mail.escuelaing.edu.co>
2024-12-07 10:19:00 -0500

    Prueba

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     coverageDirectory: './coverage',
     collectCoverage: true,
-    coverageReporters: ["lcov", "text"],
+    coverageReporters: ['lcov', 'text-summary'],
     collectCoverageFrom: ['js/home.js', 'js/sala.js', 'js/login.js'],
 };
